9daa61efc13f1ec1ba76d808a84b4e6663408744
Michael Boyko <samaranin2912@gmail.com>
2019-12-10 12:44:08 +0200

    added new feature: triple collocation validation

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='sm_tools',
-    version='0.1',
+    version='0.2',
     packages=find_packages(exclude=['tests*', 'examples']),
     install_requires=required,
     license='MIT',
