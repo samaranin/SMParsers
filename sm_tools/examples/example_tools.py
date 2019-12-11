612955adb18f2111b38c69c9402c2c15d6f0c942
Michael Boyko <samaranin2912@gmail.com>
2019-12-11 13:47:12 +0200

    added usage example for validation

--- sm_tools/examples/example_tools.py
+++ sm_tools/examples/example_tools.py
@@ -1,30 +1,33 @@
-from sm_tools.validation_tools import *
+from sm_tools.validation_tools import get_all_validation_values
 import json
 import glob
 
 
-if __name__ == "__main__":
-    # default_data_files_list = glob.glob("data/*.json")
-    #
-    # print("Example of triple collocation validation: ")
-    # for file_name in default_data_files_list:
-    #     print(f"\nGet dataset from file \'{file_name}\'", end="\n")
-    #     with open(file_name) as file:
-    #         data = json.loads(file.read())
-    #
-    #     print("  Extracted data: ")
-    #     print("    {")
-    #     for key, value in data.items():
-    #         print(f'      \'{key}\': \'{value}\'')
-    #     print("    }", end="\n")
-    #
-    #     print("Triple collocation errors: ")
-    #     for scale in (True, False):
-    #         print(f"  \'{'With' if scale else 'Without'}\' mean-standard deviation scaling: ")
-    #         e_ground, e_satellite, e_model = SMValidator.triple_collocation(data['ground_station'], data['satellite'],
-    #                                                                         data['model'], scale=scale)
-    #         print(f"    Estimated error of ground station data: {e_ground:.4f}")
-    #         print(f"    Estimated error of satellite data estimated: {e_satellite:.4f}")
-    #         print(f"    Estimated error of model data estimated: {e_model:.4f}")
+if __name__ == '__main__':
+    default_data_files_list = glob.glob('data/*.json')
 
-    print(get_all_validation_values([0, 2, 7, 5, 9], [0, 4, 7, 7, 5]))
+    print('Example of validation usage: ')
+    for file_name in default_data_files_list:
+        print(f'\nGet dataset from file \'{file_name}\'', end='\n')
+        with open(file_name) as file:
+            data = json.loads(file.read())
+
+        print('  Extracted data: ')
+        print('  {')
+        for key, value in data.items():
+            print(f'    \'{key}\': {value}')
+        print('  }', end='\n\n')
+
+        print('  Validation data:')
+        print('  {')
+        validation_data = get_all_validation_values(data['ground_station'], data['model'],
+                                                    satellite_data=data['satellite'], scale=True)
+        for method, data in validation_data.items():
+            if isinstance(data, dict):
+                print(f'    \'{method}\': {{')
+                for key, value in data.items():
+                    print(f'      \'{key}\': {value:.6f}')
+                print('    }')
+            else:
+                print(f'    \'{method}\': {data:.6f}')
+        print('  }', end='\n')
